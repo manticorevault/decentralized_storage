6e6330e12d215bdc424d55824bfc31137a93eef2
manticorevault <artur_seilah@hotmail.com>
2020-11-17 12:46:08 +0000

    Finished the Smart Contract and migrated it to ganache

--- src/contracts/Dbox.sol
+++ src/contracts/Dbox.sol
@@ -23,6 +23,16 @@ contract Dbox {
   }
 
   // Event
+  event FileUpload(
+    uint fileId,
+    string fileHash,
+    uint fileSize,
+    string fileType,
+    string fileName,
+    string fileDescription,
+    uint uploadTime,
+    address payable uploader
+  );
 
   constructor() public {
 
@@ -31,29 +41,32 @@ contract Dbox {
   // Upload File function
   function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
 
-    // Update the fileCount
-    fileCount++
-
-    // Create a new File Struct
-    files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender)
-
     // Make sure the file hash exists
+    require(bytes(_fileHash).length > 0);
     
     // Make sure file type exists
+    require(bytes(_fileType).length > 0);
 
     // Make sure file description exists
+    require(bytes(_fileDescription).length > 0);
 
     // Make sure file fileName exists
+    require(bytes(_fileName).length > 0);
 
     // Make sure uploader address exists
+    require(msg.sender != address(0));
 
     // Make sure file size is more than 0
+    require(_fileSize > 0);
 
-    // Increment file id
+    // Update the fileCount
+    fileCount++;
 
-    // Add File to the contract
+    // Create a new File Struct and add it to the Map
+    files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
 
     // Trigger an event
+    emit FileUpload(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
 
   }
 
