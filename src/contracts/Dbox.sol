9b04af2b2c8ea6db332a60115bf9ba31cb3f66b6
manticorevault <artur_seilah@hotmail.com>
2020-11-17 12:34:46 +0000

    Finished the basic MVP Smart Contract

--- src/contracts/Dbox.sol
+++ src/contracts/Dbox.sol
@@ -5,12 +5,22 @@ contract Dbox {
   string public name = "Dbox";
 
   // Number of files
-  
+  uint public fileCount = 0;
 
-  // Mapping fileId=>Struct 
+  // Mapping fileId => Struct 
+  mapping (uint => File) public files;
 
-  // Struct
-
+  // File Struct
+  struct File {
+    uint fileId;
+    string fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime;
+    address payable uploader;
+  }
 
   // Event
 
@@ -19,9 +29,16 @@ contract Dbox {
   }
 
   // Upload File function
+  function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
+
+    // Update the fileCount
+    fileCount++
+
+    // Create a new File Struct
+    files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender)
 
     // Make sure the file hash exists
-
+    
     // Make sure file type exists
 
     // Make sure file description exists
@@ -32,11 +49,14 @@ contract Dbox {
 
     // Make sure file size is more than 0
 
-
     // Increment file id
 
     // Add File to the contract
 
     // Trigger an event
+
+  }
+
+
 
 }
